e171e1676e91ee5b1ea1cc0b1f93c136c0f54698
HP 840 <yasin80545859@.com>
2022-05-10 10:48:19 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Navber from './Pages/Shared/Navber/Navber';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Aboute from './Pages/Aboute/Aboute';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navber></Navber>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/aboute' element={<Aboute></Aboute>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
